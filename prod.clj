f39b04b73be4603eac7150fdcec06d57be2f6b85
oakes <zach@sekao.net>
2019-02-16 17:15:10 -0600

    Remove lein-tools-deps

--- prod.clj
+++ prod.clj
@@ -8,7 +8,27 @@
   (p/ensure-dynamic-classloader)
   (-> "project.clj" load-file var-get))
 
-(def project (p/init-project (read-project-clj)))
+(defn read-deps-edn [aliases-to-include]
+  (let [{:keys [paths deps aliases]} (-> "deps.edn" slurp clojure.edn/read-string)
+        deps (->> (select-keys aliases aliases-to-include)
+                  vals
+                  (mapcat :extra-deps)
+                  (into deps)
+                  (reduce
+                    (fn [deps [artifact info]]
+                      (if-let [version (:mvn/version info)]
+                        (conj deps
+                          (transduce cat conj [artifact version]
+                            (select-keys info [:scope :exclusions])))
+                        deps))
+                    []))]
+    {:dependencies deps
+     :source-paths []
+     :resource-paths paths}))
+
+(def project (-> (read-project-clj)
+                 (merge (read-deps-edn []))
+                 p/init-project))
 
 (clean project)
 
--- project.clj
+++ project.clj
@@ -1,6 +1,3 @@
 (defproject full-stack-clj-example "0.1.0-SNAPSHOT"
-  :plugins [[lein-tools-deps "0.4.3"]]
-  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
-  :lein-tools-deps/config {:config-files [:install :user :project]}
   :aot [full-stack-clj-example.core]
   :main full-stack-clj-example.core)
