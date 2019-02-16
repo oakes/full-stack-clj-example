9a70a972b5c60a8bdeaa1ac4b69828181926b096
oakes <zach@sekao.net>
2019-02-16 10:59:32 -0600

    Clean project before building

--- prod.clj
+++ prod.clj
@@ -1,16 +1,22 @@
 (require
   '[cljs.build.api :as api]
   '[leiningen.core.project :as p :refer [defproject]]
+  '[leiningen.clean :refer [clean]]
   '[leiningen.uberjar :refer [uberjar]])
 
 (defn read-project-clj []
   (p/ensure-dynamic-classloader)
   (-> "project.clj" load-file var-get))
 
+(def project (p/init-project (read-project-clj)))
+
+(clean project)
+
 (println "Building main.js")
 (api/build "src" {:main          'full-stack-clj-example.core
                   :optimizations :advanced
                   :output-to     "target-cljs/public/main.js"
                   :output-dir    "target/main.out"})
+
 (println "Building uberjar")
-(-> (read-project-clj) p/init-project uberjar)
+(uberjar project)
