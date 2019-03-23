654771d1d806d47f56a79459338156623f5ffc4a
oakes <zach@sekao.net>
2019-03-23 14:25:38 -0600

    Improve read-deps-edn fn and change target dir

--- prod.clj
+++ prod.clj
@@ -1,7 +1,8 @@
 (require
+  '[clojure.java.io :as io]
+  '[clojure.string :as str]
   '[cljs.build.api :as api]
   '[leiningen.core.project :as p :refer [defproject]]
-  '[leiningen.clean :refer [clean]]
   '[leiningen.uberjar :refer [uberjar]])
 
 (defn read-project-clj []
@@ -14,29 +15,49 @@
                   vals
                   (mapcat :extra-deps)
                   (into deps)
+                  (map (fn parse-coord [coord]
+                         (let [[artifact info] coord
+                               s (str artifact)]
+                           (if-let [i (str/index-of s "$")]
+                             [(symbol (subs s 0 i))
+                              (assoc info :classifier (subs s (inc i)))]
+                             coord))))
                   (reduce
                     (fn [deps [artifact info]]
                       (if-let [version (:mvn/version info)]
                         (conj deps
                           (transduce cat conj [artifact version]
-                            (select-keys info [:scope :exclusions])))
+                            (select-keys info [:exclusions :classifier])))
                         deps))
-                    []))]
+                    []))
+        paths (->> (select-keys aliases aliases-to-include)
+                   vals
+                   (mapcat :extra-paths)
+                   (into paths))]
     {:dependencies deps
      :source-paths []
      :resource-paths paths}))
+
+(defn delete-children-recursively! [f]
+  (when (.isDirectory f)
+    (doseq [f2 (.listFiles f)]
+      (delete-children-recursively! f2)))
+  (when (.exists f) (io/delete-file f)))
 
 (def project (-> (read-project-clj)
                  (merge (read-deps-edn []))
                  p/init-project))
 
-(clean project)
+(def out-file "resources/public/main.js")
+(def out-dir "resources/public/main.out")
 
-(println "Building main.js")
+(println "Building" out-file)
+(delete-children-recursively! (io/file out-dir))
 (api/build "src" {:main          'full-stack-clj-example.core
                   :optimizations :advanced
-                  :output-to     "target-js/public/main.js"
-                  :output-dir    "target/public/main.out"})
+                  :output-to     out-file
+                  :output-dir    out-dir})
+(delete-children-recursively! (io/file out-dir))
 
 (println "Building uberjar")
 (uberjar project)
