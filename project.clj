a40361d384e93f309cb0ecf268b84baf489d300c
oakes <zach@sekao.net>
2020-08-31 18:20:59 -0400

    Add jar exclusions

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject full-stack-clj-example "0.1.0-SNAPSHOT"
   :aot [full-stack-clj-example.start]
-  :main full-stack-clj-example.start)
+  :main full-stack-clj-example.start
+  :exclusions [ring/ring-devel])
